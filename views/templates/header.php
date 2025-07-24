3b8189a8ca33434311dbfdd08bce6d41850c9dbd
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-24 12:51:51 -0500

    Update header.php

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,44 +1,24 @@
-<link href="https://fonts.cdnfonts.com/css/dear-story" rel="stylesheet">
 
 
-<div class="header">
-    <div class="header__contenedor">
-        <nav class="header__navegacion">
-            <?php if (isAuth()): ?>
-                <a href="<?php echo isAdmin() ? '/dashboard' : '/finalizar-registro'?>" class="header__enlace"> Administrar</a>
-                <form method="POST" action="/sitioweb/logout" class="header__form">
-                    <input type="submit" value="Cerrar Sesión" class="header__submit">
-                </form>
 
-            <?php else: ?>
-                <a href="/registro" class="header__enlace">Registro</a>
-                <a href="/" class="header__enlace">Iniciar Sesión</a>
-            <?php endif; ?>
 
-        </nav>
-        <div class="header__contenido">
-            <a href="">
-                <h1 class="header__logo">
-                    MEGASTOCK
-                </h1>
-            </a>
-            <p class="header__texto"> <?php dias(); ?></p>
-            <p class="header__texto header__texto--modalidad"> EN-LINEA</p>
 
-        </div>
-    </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="barra">
-    <div class="barra__contenido">
-        <a href="">
-            <h2 class="barra__logo"> MEGASTOCK </h2>
-        </a>
-        <nav class="navegacion">
-            <a href="/" class="navegacion__enlace <?php echo pagina_actual('/') ? 'navegacion__enlace--actual' : ''; ?>">Iniciar Sesion</a>
-            <a href="/registro" class="navegacion__enlace <?php echo pagina_actual('/registro') ? 'navegacion__enlace--actual' : ''; ?>">Registro</a>
-            <a href="/olvide" class="navegacion__enlace <?php echo pagina_actual('/olvide') ? 'navegacion__enlace--actual' : ''; ?>">Olvide</a>
-            <a href="/ver" class="navegacion__enlace <?php echo pagina_actual('/ver') ? 'navegacion__enlace--actual' : ''; ?>">Ver</a>
-        </nav>
-    </div>
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Mazer Admin Dashboard</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <link rel="stylesheet" href="assets/vendors/iconly/bold.css">
+
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+</head>
