0e02c23ffc1df3299943a45ebe41b121236d83e7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-24 14:26:49 -0500

    Update login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -46,7 +46,7 @@
                         <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
                     </form>
                     <div class="text-center mt-5 text-lg fs-4">
-                        <p class="text-gray-600">Don't have an account? <a href="auth-register.html"
+                        <p class="text-gray-600">Don't have an account? <a href="/auth-register"
                                 class="font-bold">Sign
                                 up</a>.</p>
                         <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
