0119d0e8daf4192e91894395a35f5920d5d3be24
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-24 12:53:36 -0500

    Update footer.php

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,44 +1,10 @@
-<footer class="footer">
-    <div class="footer__grid">
-        <div class="footer__contenido">
-            <h3 class="footer__logo">
-                MEGASTOCK
-            </h3>
-            <p class="footer__texto">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Nullam nec
-                ultricies
-                nunc. Nullam nec ultricies nunc.
-            </p>
-        </div>
+<script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-        <nav class="menu-redes">
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://facebook.com/">
-                <span class="menu-redes__ocultar">Facebook</span>
-            </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/">
-                <span class="menu-redes__ocultar">Twitter</span>
-            </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://youtube.com/">
-                <span class="menu-redes__ocultar">YouTube</span>
-            </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://instagram.com/">
-                <span class="menu-redes__ocultar">Instagram</span>
-            </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/">
-                <span class="menu-redes__ocultar">Tiktok</span>
-            </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/">
-                <span class="menu-redes__ocultar">Tiktok</span>
-            </a>
-        </nav>
+    <script src="assets/vendors/apexcharts/apexcharts.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
 
-    </div>
-    
-    <p class="footer__copyright">
-        <span class="footer__copyright--regular">
-            -Todos los derechos reservados <?php dias() ?>
-        </span>
+    <script src="assets/js/main.js"></script>
+</body>
 
-    </p>
-
-</footer>
+</html>
