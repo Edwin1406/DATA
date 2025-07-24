242098e24a3857e2085a0cbbe10e8b57aed25045
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-24 13:02:03 -0500

    Update footer.php

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,4 +1,7 @@
 <script src="../../assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<?php 
+            include_once __DIR__ .'/assets/js/bootstrap.bundle.min.js';?>
+
     <script src="../../assets/js/bootstrap.bundle.min.js"></script>
 
     <script src="../../assets/vendors/apexcharts/apexcharts.js"></script>
