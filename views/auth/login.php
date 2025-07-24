a8e905cb157c715f33417b7bbf7e9a7238cd8333
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-24 14:17:30 -0500

    Update login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,41 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo ?? '' ?></h2>
+    <p class="auth__texto">Inicia Sesion en Sitio Web </p>
+    <?php require_once __DIR__. '/../templates/alertas.php'; ?>
+
+
+    <form  class="formulario" method="POST" action="/">
+        <div class="formulario__campo">
+            <label for="email"  class="formulario__label">Email</label>
+            <input 
+                type="email" 
+                name="email" 
+                class="formulario__input" 
+                placeholder="Tu Email" 
+                id="email"
+                >
+        </div>
+        <div class="formulario__campo">
+            <label for="password"  class="formulario__label">Password</label>
+            <input 
+                type="password" 
+                name="password" 
+                class="formulario__input" 
+                placeholder="Tu password" 
+                id="password"
+                >
+        </div>
+        <input type="submit" value="Iniciar Sesion" class="formulario__submit">
+    </form>
+    <div class="acciones">
+        <a href="/olvide" class="acciones__enlace">Olvide mi password</a>
+        <a href="/registro" class="acciones__enlace">Crear Cuenta</a>
+    </div>
+</main>
+
+
+
+
 <div id="auth">
 
         <div class="row h-100">
@@ -11,7 +49,8 @@
 
                     <form action="index.html">
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" placeholder="Username">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control form-control-xl" placeholder="email">
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                             </div>
