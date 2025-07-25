32f73a1d58b5670e454e36f77c84f15194fa3400
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 16:51:32 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -804,8 +804,8 @@
             </footer>
         </div>
     </div>
-    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     
 
