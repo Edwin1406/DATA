bd49f6c384ed43f0e8aa1bdec911174d3b9d7c9a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 11:14:51 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -614,7 +614,7 @@
                                 </div>
                                 <div class="recent-message d-flex px-4 py-3">
                                     <div class="avatar avatar-lg">
-                                        <img src="assets/images/faces/1.jpg">
+                                        <img src="/assets/images/faces/1.jpg">
                                     </div>
                                     <div class="name ms-4">
                                         <h5 class="mb-1">John Dodol</h5>
