df7fb87a0b7dcff78c02bb273c57a2652ac1087b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 09:31:49 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -576,8 +576,8 @@
                                         <img src="/assets/images/faces/1.jpg" alt="Face 1">
                                     </div>
                                     <div class="ms-3 name">
-                                        <h5 class="font-bold">John Duck</h5>
-                                        <h6 class="text-muted mb-0">@johnducky</h6>
+                                        <h5 class="font-bold"><?php echo $nombre; ?></h5>
+                                        <h6 class="text-muted mb-0"><?php echo $email; ?></h6>
                                     </div>
                                 </div>
                             </div>
