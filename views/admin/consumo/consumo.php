d3d6a6aa65838c2b1423f63d6652e87522109f00
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 16:49:33 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -804,9 +804,10 @@
             </footer>
         </div>
     </div>
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-
-
+    <script src="assets/js/main.js"></script>
 </body>
 
 </html>
