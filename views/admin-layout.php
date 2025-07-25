72e694f27c86dfa5efcc8a43d0e35f585e1543e7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 13:22:01 -0500

    d

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -1,6 +1,8 @@
 <?php include_once __DIR__ . '/templates/admin-header.php'; ?>
 
+
 <main>
+      <?php include_once __DIR__ . '/templates/sidebar-only.php'; ?>
     <?php echo $contenido; ?>
 </main>
 
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,5 +1,4 @@
     <div id="app">
-       <?php include_once __DIR__ . '/templates/sidebar-only.php'; ?>
         <div id="main">
             <header class="mb-3">
                 <a href="#" class="burger-btn d-block d-xl-none">
