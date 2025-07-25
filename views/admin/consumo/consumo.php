72d2936e22d4a9d64866684d51c7fdb9355de46f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 13:09:25 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -1,8 +1,3 @@
 
+    <h2>hola</h2>
 
- <?php include __DIR__ . '/views/admin/consumo/consumo.php';?>      
-            <?php $contenido = ob_get_clean(); ?>
-
-<?php include __DIR__ . '/views/admin/admin-layout.php'?>
-
-    
