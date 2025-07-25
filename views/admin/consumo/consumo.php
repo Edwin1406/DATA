d48f0874a5ab215a9ffd77c1c8f8ab7bec5fb2f2
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 16:53:34 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -809,7 +809,7 @@
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     
 
-    <script src="assets/js/main.js"></script>
+    <script src="/assets/js/main.js"></script>
 </body>
 
 </html>
