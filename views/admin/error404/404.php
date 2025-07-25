94fe15b044e2a970272b97aab95ff5bad8dbb10b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 12:00:57 -0500

    Update 404.php

--- views/admin/error404/404.php
+++ views/admin/error404/404.php
@@ -20,9 +20,9 @@
             <div class="col-md-8 col-12 offset-md-2">
                 <img class="img-error" src="/assets/images/samples/error-404.png" alt="Not Found">
                 <div class="text-center">
-                    <h1 class="error-title">NOT FOUND</h1>
-                    <p class='fs-5 text-gray-600'>The page you are looking not found.</p>
-                    <a href="index.html" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                    <h1 class="error-title">EXTRAVIADO</h1>
+                    <p class='fs-5 text-gray-600'>La página que estás buscando no se encontró.</p>
+                    <a href="/admin/index" class="btn btn-lg btn-outline-primary mt-3">Volver al inicio</a>
                 </div>
             </div>
         </div>
