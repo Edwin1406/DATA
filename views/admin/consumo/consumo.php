309673866a212e0bb478cd384715a1d69bff7940
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 12:57:36 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -1,4 +1,5 @@
- <?php include_once __DIR__ .'views/templates/admin-header.php';?>      
-        
 
-<?php include_once __DIR__ .'views/templates/admin-sidebar.php';?>
+
+ <?php include_once(__DIR__ . '/../templates/admin-header.php');?>
+ <?php include_once(__DIR__ . '/../templates/admin-sidebar.php');?>
+
