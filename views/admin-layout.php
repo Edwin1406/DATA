aa80f0f4dc87a0bf132543af24c21f120a814303
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 12:10:26 -0500

    .

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -1,13 +1,8 @@
 
 
- <?php 
-            include_once __DIR__ .'/templates/admin-header.php';
-        ?>
-      
+ <?php include_once __DIR__ .'/templates/admin-header.php';?>      
             <?php echo $contenido; ?>
-    
- <?php 
-            include_once __DIR__ .'/templates/admin-sidebar.php';
-        ?>
+
+<?php include_once __DIR__ .'/templates/admin-sidebar.php';?>
 
     
---
+++ views/admin/consumo/consumo.php
@@ -0,0 +1,4 @@
+ <?php include_once __DIR__ .'/templates/admin-header.php';?>      
+        
+
+<?php include_once __DIR__ .'/templates/admin-sidebar.php';?>
