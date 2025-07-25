bb377a0962b20ae228fa22032988865be79db8a3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 11:27:43 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -642,11 +642,11 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2021 &copy; Mazer</p>
+                        <p>2025 &copy; EDWIN DIAZ</p>
                     </div>
                     <div class="float-end">
                         <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                                href="http://ahmadsaugi.com">A. Saugi</a></p>
+                                href="">A. Saugi</a></p>
                     </div>
                 </div>
             </footer>
