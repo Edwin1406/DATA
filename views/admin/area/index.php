616aa4197d1735d283014806d63dbf0558bb425c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 11:14:03 -0500

    .

--- views/admin/area/index.php
+++
@@ -1,9 +0,0 @@
-<h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
-
-<div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/area/crear" >
-        <i class="fa-solid fa-circle-arrow-left"></i>
-        Anadir area
-    </a>
-
-</div>
--- views/admin/area/paginaArea.php
+++
@@ -1,1 +0,0 @@
-<h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
