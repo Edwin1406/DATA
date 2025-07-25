6c6f93176bb25288fda855a54f2b9ea0bc28f232
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 16:27:28 -0500

    .

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -56,20 +56,8 @@
                         <div class="card">
                             <div class="card-header">
                                 <h4 class="card-title">Registro de Papel</h4>
-                                   
-     <?php if (!empty($alertas)) : ?>
-    <?php foreach ($alertas as $tipo => $mensajes) : ?>
-        <?php 
-            $color = $tipo === 'error' ? 'danger' : $tipo;
-        ?>
-        <?php foreach ($mensajes as $mensaje) : ?>
-            <div class="border-start border-4 border-<?php echo $color; ?> ps-3 py-2 mb-3 bg-light text-<?php echo $color; ?> rounded">
-                <?php echo $mensaje; ?>
-            </div>
-        <?php endforeach; ?>
-    <?php endforeach; ?>
-<?php endif; ?>
-
+                                   <?php include_once __DIR__ . '/../../templates/alertas.php'  ?>
+                          
 
                             </div>
                             <div class="card-content">
