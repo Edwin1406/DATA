c89e0caeacc53bb5457a593833fc765bc2d42fd8
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-25 13:01:09 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -6,3 +6,4 @@
  <?php include_once(__DIR__ . '../../templates/admin-sidebar.php');?>
 
 
+<?php include_once('/home/u904102484/domains/pruebas.megawebsistem.com/public_html/views/admin/consumoviews/templates/admin-header.php');?>
