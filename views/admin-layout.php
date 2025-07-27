75458cdfed435bb295f332f6224f8246430499fb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-27 12:21:00 -0500

    Update admin-layout.php

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -7,4 +7,4 @@
         <?php echo $contenido; ?>
     </div>
 </div>
-<?php include_once __DIR__ . '/templates/admin-footer.php'; ?>
+<?php include_once __DIR__ . '/templates/admin-sidebar.php'; ?>
