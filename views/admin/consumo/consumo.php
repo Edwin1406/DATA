9f83a8a0a6a1ea193fe3ba3e6b0c5a75ceda4006
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-27 12:25:47 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -1,4 +1,10 @@
-
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="/assets/css/bootstrap.css">
+<link rel="stylesheet" href="/assets/vendors/iconly/bold.css">
+<link rel="stylesheet" href="/assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+<link rel="stylesheet" href="/assets/vendors/bootstrap-icons/bootstrap-icons.css">
+<link rel="stylesheet" href="/assets/css/app.css">
+<link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/x-icon">
         <div class="page-heading">
             <div class="page-title">
                 <div class="row">
