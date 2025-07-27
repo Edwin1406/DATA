047bc3c7f9e29b95bca6975ac43ab8084d56bc80
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-27 12:21:39 -0500

    Update admin-layout.php

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -1,6 +1,5 @@
 <!-- layout.php -->
 <?php include_once __DIR__ . '/templates/admin-header.php'; ?>
-<?php include_once __DIR__ . '/templates/admin-sidebar.php'; ?>
 
 <div id="app">
     <div id="main">
