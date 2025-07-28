40b65fa0597f1b4f56fcd0f570ddaaf63489b898
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-28 16:40:38 -0500

    Update admin-header.php

--- views/templates/admin-header.php
+++ views/templates/admin-header.php
@@ -17,6 +17,8 @@
 <link rel="stylesheet" href="/assets/vendors/bootstrap-icons/bootstrap-icons.css">
 <link rel="stylesheet" href="/assets/css/app.css">
 <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="/assets/vendors/choices.js/choices.min.css" />
+
 
 </head>
 
