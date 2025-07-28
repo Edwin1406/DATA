203b51eea44f7df62b6956de0734d50e4a1a82ed
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-27 19:53:02 -0500

    Update admin-layout.php

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -10,3 +10,11 @@
         <?php include_once __DIR__ . '/templates/admin-sidebar.php'; ?>
     </div>
 </div>
+
+<script>
+if (window.location.pathname === "/admin/dashboard") {
+    var s = document.createElement('script');
+    s.src = "/assets/js/pages/dashboard.js";
+    document.body.appendChild(s);
+}
+</script>
