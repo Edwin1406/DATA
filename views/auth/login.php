a359327148c4811929c1a60c40ab4c9df9f620a1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-27 22:03:15 -0500

    Update login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -10,8 +10,8 @@
                     <div class="auth-logo">
                         <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo"></a>
                     </div>
-                    <h1 class="auth-title">Log in.</h1>
-                    <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
+                    <h1 class="auth-title">Acceso.</h1>
+                    <p class="auth-subtitle mb-5">Inicie sesión con los datos que ingresó durante el registro.</p>
 
                     <form action="/" method="POST">
                         <div class="form-group position-relative has-icon-left mb-4">
@@ -41,16 +41,15 @@
                         <div class="form-check form-check-lg d-flex align-items-end">
                             <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
                             <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                                Keep me logged in
+                                Mantenerme conectado
                             </label>
                         </div>
-                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Iniciar Sesión</button>
                     </form>
                     <div class="text-center mt-5 text-lg fs-4">
-                        <p class="text-gray-600">Don't have an account? <a href="/registro"
-                                class="font-bold">Sign
-                                up</a>.</p>
-                        <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
+                        <p class="text-gray-600">¿No tienes una cuenta? <a href="/registro"
+                                class="font-bold">Regístrate</a>.</p>
+                        <p><a class="font-bold" href="auth-forgot-password.html">¿Olvidaste tu contraseña?</a>.</p>
                     </div>
                 </div>
             </div>
