35574c9e4be6ac649075014b502331234c2c3c24
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 11:28:41 -0500

    .

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,9 +1,3 @@
-<header class="mb-3">
-    <a href="#" class="burger-btn d-block d-xl-none">
-        <i class="bi bi-justify fs-3"></i>
-    </a>
-</header>
-
 
 <footer>
     <div class="footer clearfix mb-0 text-muted">
