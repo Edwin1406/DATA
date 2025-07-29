42fe76cbcaf8dde13cd7dde464c1adcf4051034c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 12:00:17 -0500

    Update sidebar-only.php

--- views/templates/sidebar-only.php
+++ views/templates/sidebar-only.php
@@ -3,7 +3,8 @@
          <i class="bi bi-justify fs-3"></i>
      </a>
  </header>
- <div id="sidebar" class="active">
+ <!-- SIDEBAR -->
+<div id="sidebar" class="active">
     <div class="sidebar-wrapper active">
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
@@ -24,80 +25,50 @@
                 <li class="sidebar-title">Menú</li>
 
                 <!-- Dashboard -->
-                <li class="sidebar-item active">
+                <li class="sidebar-item">
                     <a href="/admin/index" class="sidebar-link">
                         <i class="bi bi-grid-fill"></i>
                         <span>Dashboard</span>
                     </a>
                 </li>
 
-                <!-- Producción -->
+                <!-- PRODUCCIÓN -->
                 <li class="sidebar-item has-sub">
                     <a href="#" class="sidebar-link">
                         <i class="bi bi-stack"></i>
                         <span>PRODUCCIÓN</span>
                     </a>
                     <ul class="submenu">
-                        <!-- Consumo Papel y sus opciones -->
-                        <li class="submenu-item">
-                            <a href="/admin/consumo">Consumo Papel</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="/admin/consumo/registro">➤ Registro</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="/admin/consumo/reporte">➤ Reporte</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="/admin/consumo/historial">➤ Historial</a>
+
+                        <!-- Submenú: Consumo Papel -->
+                        <li class="submenu-item has-sub">
+                            <a href="#" class="submenu-link">Consumo Papel</a>
+                            <ul class="submenu sub-submenu">
+                                <li class="submenu-item">
+                                    <a href="/admin/consumo/registro">Registro</a>
+                                </li>
+                                <li class="submenu-item">
+                                    <a href="/admin/consumo/reporte">Reporte</a>
+                                </li>
+                                <li class="submenu-item">
+                                    <a href="/admin/consumo/historial">Historial</a>
+                                </li>
+                            </ul>
                         </li>
 
-                        <!-- Otros ítems de ejemplo -->
+                        <!-- Otros ítems de PRODUCCIÓN -->
                         <li class="submenu-item">
                             <a href="component-badge.html">Badge</a>
                         </li>
                         <li class="submenu-item">
-                            <a href="component-breadcrumb.html">Breadcrumb</a>
-                        </li>
-                        <li class="submenu-item">
                             <a href="component-button.html">Button</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-card.html">Card</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-carousel.html">Carousel</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-dropdown.html">Dropdown</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-list-group.html">List Group</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-modal.html">Modal</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-navs.html">Navs</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-pagination.html">Pagination</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-progress.html">Progress</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-spinner.html">Spinner</a>
-                        </li>
-                        <li class="submenu-item">
-                            <a href="component-tooltip.html">Tooltip</a>
                         </li>
                     </ul>
                 </li>
             </ul>
         </div>
 
-        <!-- Botón para cerrar menú en mobile -->
+        <!-- Sidebar toggle button -->
         <button class="sidebar-toggler btn x">
             <i data-feather="x"></i>
         </button>
