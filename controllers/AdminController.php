420b6387b0c35747f7aa9b63eb51822230950540
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 16:40:36 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -16,13 +16,13 @@ class AdminController
         if (!isset($_SESSION['email'])) {
             header('Location: /');
         }
-            $usuariosConectados = self::contarUsuariosConectados();
+        $usuariosConectados = self::contarUsuariosConectados();
 
         // NOMBRE DE LA PERSONA LOGEADA
         $nombre = $_SESSION['nombre'];
         $email = $_SESSION['email'];
         // debuguear($nombre);
-        $router->render('admin/dashboard/index' , [
+        $router->render('admin/dashboard/index', [
             'titulo' => 'MEGASTOCK-DESARROLLO',
             'nombre' => $nombre,
             'email' => $email,
@@ -39,21 +39,21 @@ class AdminController
         }
         // NOMBRE DE LA PERSONA LOGEADA
         $nombre = $_SESSION['nombre'];
-      //cerrar sesión 
-      
+        //cerrar sesión 
+
 
 
         $alertas = [];
         $consumo = new Prueba();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            
+
             if (isset($_POST['personal']) && is_array($_POST['personal'])) {
                 $_POST['personal'] = implode(',', $_POST['personal']);
             }
             $consumo->sincronizar($_POST);
             $consumo->sacarTotalHoras();
 
-            
+
             // Calcular productividad cada 15 minutos
             $cantidad = is_numeric($consumo->cantidad) ? (float)$consumo->cantidad : 0;
             $minutos_trabajados = $consumo->total_horas * 60;
@@ -61,7 +61,6 @@ class AdminController
             if ($cantidad > 0 && $minutos_trabajados > 0) {
                 // $control->x_hora = ($cantidad / $minutos_trabajados) * 15;
                 $consumo->x_hora = round(($cantidad / $minutos_trabajados) * 15);
-
             } else {
                 $consumo->x_hora = 0;
             }
@@ -71,12 +70,10 @@ class AdminController
             if (empty($alertas)) {
                 $consumo->guardar();
                 // header('Location: /admin/consumo');
-                    header('Location: /admin/consumo?exito=1');
-
+                header('Location: /admin/consumo?exito=1');
             }
         } else {
             $alertas = [];
-
         }
 
         $router->render('admin/consumo/consumo', [
@@ -88,27 +85,27 @@ class AdminController
 
 
 
-// tabla de consumo
-public static function tablaConsumo(Router $router)
-{
-    session_start();
-    if (!isset($_SESSION['email'])) {
-        header('Location: /');
+    // tabla de consumo
+    public static function tablaConsumo(Router $router)
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+        }
+        // NOMBRE DE LA PERSONA LOGEADA
+        $nombre = $_SESSION['nombre'];
+        $email = $_SESSION['email'];
+
+        $consumos = Prueba::all();
+        // debuguear($consumos);
+
+        $router->render('admin/consumo/tablaConsumo', [
+            'titulo' => 'MEGASTOCK-DESARROLLO',
+            'nombre' => $nombre,
+            'email' => $email,
+            'consumos' => $consumos
+        ]);
     }
-    // NOMBRE DE LA PERSONA LOGEADA
-    $nombre = $_SESSION['nombre'];
-    $email = $_SESSION['email'];
-
-    $consumos = Prueba::all();
-    // debuguear($consumos);
-
-    $router->render('admin/consumo/tablaConsumo', [
-        'titulo' => 'MEGASTOCK-DESARROLLO',
-        'nombre' => $nombre,
-        'email' => $email,
-        'consumos' => $consumos
-    ]);
-}
 
 
     // Eliminar consumo
@@ -150,7 +147,7 @@ public static function tablaConsumo(Router $router)
         $nombre = $_SESSION['nombre'];
         $email = $_SESSION['email'];
         $consumo_general = new Consumo_general;
-        
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $tipo_maquina = $_POST['tipo_maquina'] ?? '';
             $consumo_general->sincronizar($_POST);
@@ -169,98 +166,97 @@ public static function tablaConsumo(Router $router)
             'titulo' => 'MEGASTOCK-DESARROLLO',
             'nombre' => $nombre,
             'email' => $email,
-          
+
         ]);
-
     }
 
 
-// TABLA CONSUMO GENERAL
+    // TABLA CONSUMO GENERAL
 
 
-public static function tablaConsumoGeneral(Router $router)
-{
-    session_start();
-    if (!isset($_SESSION['email'])) {
-        header('Location: /');
-    }
-    // NOMBRE DE LA PERSONA LOGEADA
-    $nombre = $_SESSION['nombre'];
-    $email = $_SESSION['email'];
+    public static function tablaConsumoGeneral(Router $router)
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+        }
+        // NOMBRE DE LA PERSONA LOGEADA
+        $nombre = $_SESSION['nombre'];
+        $email = $_SESSION['email'];
 
-    $consumosGenerales = Consumo_general::all();
-    // debuguear($consumosGenerales);
+        $consumosGenerales = Consumo_general::all();
+        // debuguear($consumosGenerales);
 
-    $router->render('admin/consumo/tablaConsumoGeneral', [
-        'titulo' => 'MEGASTOCK-DESARROLLO',
-        'nombre' => $nombre,
-        'email' => $email,
-        'consumosGenerales' => $consumosGenerales
-    ]);
-}
-
-
-
-// ELIMINAR CONSUMO GENERAL
-public static function eliminarConsumoGeneral(Router $router)
-{
-    session_start();
-    if (!isset($_SESSION['email'])) {
-        header('Location: /');
+        $router->render('admin/consumo/tablaConsumoGeneral', [
+            'titulo' => 'MEGASTOCK-DESARROLLO',
+            'nombre' => $nombre,
+            'email' => $email,
+            'consumosGenerales' => $consumosGenerales
+        ]);
     }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $id = $_POST['id'] ?? null;
-        if ($id) {
-            $consumoGeneral = Consumo_general::find($id);
-            if ($consumoGeneral) {
-                $consumoGeneral->eliminar();
-                header('Location: /admin/tablaConsumoGeneral?exito=1');
+
+
+    // ELIMINAR CONSUMO GENERAL
+    public static function eliminarConsumoGeneral(Router $router)
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'] ?? null;
+            if ($id) {
+                $consumoGeneral = Consumo_general::find($id);
+                if ($consumoGeneral) {
+                    $consumoGeneral->eliminar();
+                    header('Location: /admin/tablaConsumoGeneral?exito=1');
+                } else {
+                    header('Location: /admin/tablaConsumoGeneral?error=1');
+                }
             } else {
                 header('Location: /admin/tablaConsumoGeneral?error=1');
             }
-        } else {
-            header('Location: /admin/tablaConsumoGeneral?error=1');
         }
     }
-}
 
 
 
 
-public static function tablaAdminConsumoGeneral(Router $router)
-{
-    session_start();
-    if (!isset($_SESSION['email'])) {
-        header('Location: /');
-    }
-    // NOMBRE DE LA PERSONA LOGEADA
-    $nombre = $_SESSION['nombre'];
-    $email = $_SESSION['email'];
+    public static function tablaAdminConsumoGeneral(Router $router)
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+        }
+        // NOMBRE DE LA PERSONA LOGEADA
+        $nombre = $_SESSION['nombre'];
+        $email = $_SESSION['email'];
 
-    $consumosGenerales = Consumo_general::all();
-    // debuguear($consumosGenerales);
+        $consumosGenerales = Consumo_general::all();
+        // debuguear($consumosGenerales);
 
-    $router->render('admin/consumo/tablaAdminConsumoGeneral', [
-        'titulo' => 'MEGASTOCK-DESARROLLO',
-        'nombre' => $nombre,
-        'email' => $email,
-        'consumosGenerales' => $consumosGenerales
-    ]);
-}
-
-
-
-
-// EDITAR CONSUMO GENERAL
-public static function editarAdminConsumoGeneral(Router $router)
-{
-    session_start();
-    if (!isset($_SESSION['email'])) {
-        header('Location: /');
+        $router->render('admin/consumo/tablaAdminConsumoGeneral', [
+            'titulo' => 'MEGASTOCK-DESARROLLO',
+            'nombre' => $nombre,
+            'email' => $email,
+            'consumosGenerales' => $consumosGenerales
+        ]);
     }
 
-       $id = $_GET['id'];
+
+
+
+    // EDITAR CONSUMO GENERAL
+    public static function editarAdminConsumoGeneral(Router $router)
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+        }
+
+        $id = $_GET['id'];
         $id = filter_var($id, FILTER_VALIDATE_INT);
 
         if (!$id) {
@@ -270,61 +266,61 @@ public static function editarAdminConsumoGeneral(Router $router)
         // debuguear($id);
 
 
-    // NOMBRE DE LA PERSONA LOGEADA
-    $nombre = $_SESSION['nombre'];
-    $email = $_SESSION['email'];
+        // NOMBRE DE LA PERSONA LOGEADA
+        $nombre = $_SESSION['nombre'];
+        $email = $_SESSION['email'];
 
-    $alertas = [];
-    $consumoGeneral = Consumo_general::find($id);
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $consumoGeneral->sincronizar($_POST);
-        // sin espacio en blanco
-        $consumoGeneral->tipo_maquina = trim($consumoGeneral->tipo_maquina);
-        // debuguear($consumoGeneral);
-        $alertas = $consumoGeneral->validar();
+        $alertas = [];
+        $consumoGeneral = Consumo_general::find($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $consumoGeneral->sincronizar($_POST);
+            // sin espacio en blanco
+            $consumoGeneral->tipo_maquina = trim($consumoGeneral->tipo_maquina);
+            // debuguear($consumoGeneral);
+            $alertas = $consumoGeneral->validar();
 
-        if (empty($alertas)) {
-            $consumoGeneral->guardar();
-            header('Location: /admin/tablaAdminConsumoGeneral?exito=1');
-        }
-    } else {
-        $id = $_GET['id'] ?? null;
-        if ($id) {
-            $consumoGeneral = Consumo_general::find($id);
-            if (!$consumoGeneral) {
-                header('Location: /admin/tablaAdminConsumoGeneral?error=1');
+            if (empty($alertas)) {
+                $consumoGeneral->guardar();
+                header('Location: /admin/tablaAdminConsumoGeneral?exito=1');
             }
         } else {
-            header('Location: /admin/tablaAdminConsumoGeneral?error=1');
+            $id = $_GET['id'] ?? null;
+            if ($id) {
+                $consumoGeneral = Consumo_general::find($id);
+                if (!$consumoGeneral) {
+                    header('Location: /admin/tablaAdminConsumoGeneral?error=1');
+                }
+            } else {
+                header('Location: /admin/tablaAdminConsumoGeneral?error=1');
+            }
         }
+
+        $router->render('admin/consumo/editarAdminConsumoGeneral', [
+            'titulo' => 'MEGASTOCK-DESARROLLO',
+            'alertas' => $alertas,
+            'nombre' => $nombre,
+            'email' => $email,
+            'consumoGeneral' => $consumoGeneral
+        ]);
     }
 
-    $router->render('admin/consumo/editarAdminConsumoGeneral', [
-        'titulo' => 'MEGASTOCK-DESARROLLO',
-        'alertas' => $alertas,
-        'nombre' => $nombre,
-        'email' => $email,
-        'consumoGeneral' => $consumoGeneral
-    ]);
-}
 
 
 
 
+    private static function contarUsuariosConectados()
+    {
+        $path = ini_get("session.save_path");
+        if (empty($path)) $path = sys_get_temp_dir();
 
-private static function contarUsuariosConectados()
-{
-    $path = ini_get("session.save_path");
-    if (empty($path)) $path = sys_get_temp_dir();
-
-    $cuenta = 0;
-    foreach (glob("$path/sess_*") as $file) {
-        if (filemtime($file) + ini_get("session.gc_maxlifetime") > time()) {
-            $cuenta++;
+        $cuenta = 0;
+        foreach (glob("$path/sess_*") as $file) {
+            if (filemtime($file) + ini_get("session.gc_maxlifetime") > time()) {
+                $cuenta++;
+            }
         }
+        return $cuenta;
     }
-    return $cuenta;
-}
 
 
 
@@ -344,8 +340,4 @@ private static function contarUsuariosConectados()
             'error' => 'Página no encontrada'
         ]);
     }
-    
 }
-
-
-
