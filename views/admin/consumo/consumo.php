199c1f7ef8202beae48c69fc2c0c70486a6bb015
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-28 20:27:11 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -82,14 +82,16 @@
                                         </div>
                                     </div> -->
 
-
+<!-- 
                                     <section class="multiple-choices">
                                         <div class="row">
                                             <div class="col-12">
                                                 <div class="card">
                                                     <div class="card-header">
                                                         <h4 class="card-title">Multiple choices</h4>
-                                                    </div>
+                                                    </div> -->
+
+
                                                     <div class="card-content">
                                                         <div class="card-body">
                                                             <div class="row">
@@ -117,10 +119,14 @@
                                                             </div>
                                                         </div>
                                                     </div>
+
+
+
+<!-- 
                                                 </div>
                                             </div>
                                         </div>
-                                    </section>
+                                    </section> -->
 
 
 
