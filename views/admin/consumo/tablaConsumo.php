5830970f0cf8f9e3d4c553fa555a892f77a31834
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 08:58:21 -0500

    Update tablaConsumo.php

--- views/admin/consumo/tablaConsumo.php
+++ views/admin/consumo/tablaConsumo.php
@@ -9,7 +9,7 @@
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index.html"> <?php echo $nombre; ?></a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">DataTable</li>
+                                            <li class="breadcrumb-item"><a href="/cerrarSesion">Cerrar Sesión</a></li>
                                 </ol>
                             </nav>
                         </div>
