901a64df1bef324ef3a7172ea044db8fe458bf87
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 11:36:01 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,9 +1,9 @@
-
+<!-- 
             <header class="mb-3">
                 <a href="#" class="burger-btn d-block d-xl-none">
                     <i class="bi bi-justify fs-3"></i>
                 </a>
-            </header>
+            </header> -->
 
             <div class="page-heading">
                 <h3>ESTADISTICAS DEL PERFIL </h3>
