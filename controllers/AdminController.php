1e1198122d06ab88ca3613190188f969b8d3d496
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-28 22:02:40 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -48,8 +48,8 @@ class AdminController
             // if (isset($_POST['personal']) && is_array($_POST['personal'])) {
             //     $_POST['personal'] = implode(',', $_POST['personal']);
             // }
-            $consumo->sacarTotalHoras();
             $consumo->sincronizar($_POST);
+            $consumo->sacarTotalHoras();
 
             
             // Calcular productividad cada 15 minutos
