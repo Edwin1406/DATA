f483eaca3545fc72a07e835b8b1d70f36226a159
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 15:02:04 -0500

    Update tablaConsumoGeneral.php

--- views/admin/consumo/tablaConsumoGeneral.php
+++ views/admin/consumo/tablaConsumoGeneral.php
@@ -74,10 +74,9 @@
                                  <td><?= $consumosGeneral->created_at ?></td>
                                
                                  <!-- los botones en horizontal -->
-
                                  <td>
                                      <div class="d-flex gap-1">
-                                         <a href="/admin/editarConsumo?id=<?= $consumosGeneral->id ?>" class="btn btn-primary btn-sm">Editar</a>
+                                         <a href="/admin/editarConsumo?id=<?= $consumo->id ?>" class="btn btn-primary btn-sm">Editar</a>
                                          <form action="/admin/eliminarConsumo" method="POST">
                                              <input type="hidden" name="id" value="<?= $consumo->id ?>">
                                              <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
