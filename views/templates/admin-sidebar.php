1b4f1e26d49c036cfb6614300d3f4b6a7930853d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 11:28:03 -0500

    .

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,3 +1,10 @@
+<header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
+
+
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
