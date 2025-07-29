a16d7ab40d808ada441c62680c53ea9ca96c2183
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 10:36:26 -0500

    Update admin-sidebar.php

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -13,7 +13,7 @@
 
 <script src="/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="/assets/js/bootstrap.bundle.min.js"></script>
-<!-- <script src="/assets/vendors/simple-datatables/simple-datatables.js"></script> -->
+<script src="/assets/vendors/simple-datatables/simple-datatables.js"></script>
 
 <!-- <script src="/assets/vendors/apexcharts/apexcharts.js"></script>
 <script src="/assets/js/pages/dashboard.js"></script> -->
