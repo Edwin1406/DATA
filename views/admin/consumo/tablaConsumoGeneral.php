6481e35717ff7d06a4a74940cf7a9b4726fe914a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-30 15:38:32 -0500

    Update tablaConsumoGeneral.php

--- views/admin/consumo/tablaConsumoGeneral.php
+++ views/admin/consumo/tablaConsumoGeneral.php
@@ -23,56 +23,7 @@
                                                 role="tab" aria-controls="contact" aria-selected="false">Contact</a>
                                         </li>
                                     </ul>
-                                    <div class="tab-content" id="myTabContent">
-                                        <div class="tab-pane fade show active" id="home" role="tabpanel"
-                                            aria-labelledby="home-tab">
-                                            <p class='my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                                Nulla ut nulla
-                                                neque. Ut hendrerit nulla a euismod pretium.
-                                                Fusce venenatis sagittis ex efficitur suscipit. In tempor mattis
-                                                fringilla. Sed id
-                                                tincidunt orci, et volutpat ligula.
-                                                Aliquam sollicitudin sagittis ex, a rhoncus nisl feugiat quis. Lorem
-                                                ipsum dolor sit
-                                                amet, consectetur adipiscing elit.
-                                                Nunc ultricies ligula a tempor vulputate. Suspendisse pretium mollis
-                                                ultrices.</p>
-                                        </div>
-                                        <div class="tab-pane fade" id="profile" role="tabpanel"
-                                            aria-labelledby="profile-tab">
-                                            Integer interdum diam eleifend metus lacinia, quis gravida eros mollis.
-                                            Fusce non sapien
-                                            sit amet magna dapibus
-                                            ultrices. Morbi tincidunt magna ex, eget faucibus sapien bibendum non. Duis
-                                            a mauris ex.
-                                            Ut finibus risus sed massa
-                                            mattis porta. Aliquam sagittis massa et purus efficitur ultricies. Integer
-                                            pretium dolor
-                                            at sapien laoreet ultricies.
-                                            Fusce congue et lorem id convallis. Nulla volutpat tellus nec molestie
-                                            finibus. In nec
-                                            odio tincidunt eros finibus
-                                            ullamcorper. Ut sodales, dui nec posuere finibus, nisl sem aliquam metus, eu
-                                            accumsan
-                                            lacus felis at odio. Sed lacus
-                                            quam, convallis quis condimentum ut, accumsan congue massa. Pellentesque et
-                                            quam vel
-                                            massa pretium ullamcorper vitae eu
-                                            tortor.
-                                        </div>
-                                        <div class="tab-pane fade" id="contact" role="tabpanel"
-                                            aria-labelledby="contact-tab">
-                                            <p class="mt-2">Duis ultrices purus non eros fermentum hendrerit. Aenean
-                                                ornare interdum
-                                                viverra. Sed ut odio velit. Aenean eu diam
-                                                dictum nibh rhoncus mattis quis ac risus. Vivamus eu congue ipsum.
-                                                Maecenas id
-                                                sollicitudin ex. Cras in ex vestibulum,
-                                                posuere orci at, sollicitudin purus. Morbi mollis elementum enim, in
-                                                cursus sem
-                                                placerat ut.</p>
-                                        </div>
-                                    </div>
+                             
                                 </div>
                             </div>
 
