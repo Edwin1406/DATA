b7ee573968bcfae7f5d7f0726e2b5f8a71f169ad
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 21:08:35 -0500

    .

--- views/admin/consumo/consumo_general.php
+++ views/admin/consumo/consumo_general.php
@@ -9,7 +9,7 @@
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html"><?php echo $nombre; ?></a></li>
+                        <li class="breadcrumb-item"><a><?php echo $nombre; ?></a></li>
                         <!--  cerrar sesión -->
                         <li class="breadcrumb-item"><a href="/cerrarSesion">Cerrar Sesión</a></li>
 
