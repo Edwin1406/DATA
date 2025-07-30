9f0da413e4c889c72f49624374d2abc72f2c7643
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-29 20:30:37 -0500

    Update sidebar-only.php

--- views/templates/sidebar-only.php
+++ views/templates/sidebar-only.php
@@ -4,13 +4,12 @@
     ?>
 
 
- <div id="sidebar" class="active">
-    
  <header class="mb-3">
      <a href="" class="burger-btn d-block d-xl-none">
          <i class="bi bi-justify fs-3"></i>
      </a>
  </header>
+ <div id="sidebar" class="active">
      <div class="sidebar-wrapper active">
          <div class="sidebar-header">
              <div class="d-flex justify-content-between">
