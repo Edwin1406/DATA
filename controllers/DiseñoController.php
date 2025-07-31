5519ed51af1bbb1a85be24a62fb8effea195e3d7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-30 20:36:06 -0500

    a

--- controllers/DiseñoController.php
+++ controllers/DiseñoController.php
@@ -2,13 +2,16 @@
 
 namespace Controllers;
 
-
+use Model\Diseno;
 use MVC\Router;
 
 class DiseñoController
 {
     public static function crearDiseno(Router $router)
     {
+
+
+
         session_start();
         if (!isset($_SESSION['email'])) {
             header('Location: /');
@@ -17,6 +20,19 @@ class DiseñoController
         // NOMBRE DE LA PERSONA LOGEADA
         $nombre = $_SESSION['nombre'];
         $email = $_SESSION['email'];
+
+         $diseno = new Diseno();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $diseno->sincronizar($_POST);
+            debuguear($diseno);
+
+       
+          
+                // Manejar las alertas de validación
+            }
+
+
 
         // Aquí puedes agregar la lógica para crear un diseño
 
