982ca55fe1963ac91dc660c183242aeed79f9187
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-31 09:55:08 -0500

    Update formulario.php

--- views/admin/diseno/formulario.php
+++ views/admin/diseno/formulario.php
@@ -16,7 +16,7 @@
           <label for="proveedor">Nombre del Proveedor</label>
           <input type="text" id="proveedor" class="form-control"
               placeholder="Nombre del Proveedor" name="proveedor"
-              value="<?php echo s($diseno->proveedor); ?>">
+              value="<?php echo ($diseno->proveedor); ?>">
       </div>
   </div>
 
@@ -26,7 +26,7 @@
           <label for="nombre_producto">Nombre del Producto</label>
           <input type="text" id="nombre_producto" class="form-control"
               placeholder="Nombre del Producto" name="nombre_producto"
-              value="<?php echo s($diseno->nombre_producto); ?>">
+              value="<?php echo ($diseno->nombre_producto); ?>">
       </div>
   </div>
 
@@ -37,7 +37,7 @@
           <label for="codigo_producto">Código del Producto</label>
           <input type="text" id="codigo_producto" class="form-control"
               placeholder="Código del Producto" name="codigo_producto"
-              value="<?php echo s($diseno->codigo_producto); ?>">
+              value="<?php echo ($diseno->codigo_producto); ?>">
       </div>
   </div>
 
@@ -46,9 +46,9 @@
       <div class="form-group">
           <label for="estado">Estado</label>
           <select class="form-select" name="estado" id="estado">
-              <option value="ENVIADO" <?php echo s($diseno->estado) === 'ENVIADO' ? 'selected' : ''; ?>>Enviado</option>
-              <option value="PAUSADO" <?php echo s($diseno->estado) === 'PAUSADO' ? 'selected' : ''; ?>>Pausado</option>
-              <option value="TERMINADO" <?php echo s($diseno->estado) === 'TERMINADO' ? 'selected' : ''; ?>>Terminado</option>
+              <option value="ENVIADO" <?php echo ($diseno->estado) === 'ENVIADO' ? 'selected' : ''; ?>>Enviado</option>
+              <option value="PAUSADO" <?php echo ($diseno->estado) === 'PAUSADO' ? 'selected' : ''; ?>>Pausado</option>
+              <option value="TERMINADO" <?php echo ($diseno->estado) === 'TERMINADO' ? 'selected' : ''; ?>>Terminado</option>
           </select>
       </div>
   </div>
