385912753e4a15f33a9e33aef98e8102aa6bd9f9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-31 11:19:27 -0500

    Update formulario.php

--- views/admin/diseno/formulario.php
+++ views/admin/diseno/formulario.php
@@ -66,9 +66,13 @@
           <div class="form-group">
               <label>Archivo actual:</label><br>
               <!-- eliminar espacio em blanco -->
-              <a href="<?php echo $_ENV['HOST'] . '/src/visor/' . $diseno->pdf; ?>" target="_blank" class="btn btn-outline-primary btn-sm">
+              <!-- <a href="<?php echo $_ENV['HOST'] . '/src/visor/' . $diseno->pdf; ?>" target="_blank" class="btn btn-outline-primary btn-sm">
                   Ver / Descargar PDF
-              </a>
+              </a> -->
+              <a href="<?php echo $_ENV['HOST'] . '/src/visor/' . trim($diseno->pdf); ?>" target="_blank" class="btn btn-outline-primary btn-sm">
+    Ver / Descargar PDF
+</a>
+
               <br><br>
 
               <?php if ($diseno->pdf): ?>
