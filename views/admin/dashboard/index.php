5189e10197b57b6d2ee484af63884b3232a38c58
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-31 20:59:15 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -10,7 +10,7 @@
     </div>
 
     <div class="row">
-        <div class="col-12 col-lg-12">
+        <div class="col-12 col-lg-6">
             <div class="card">
                 <div class="card-header">
                     <h4>Filtrar por Fecha</h4>
