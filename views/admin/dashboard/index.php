6cd658367b26a166890447cf6a30b08c27b7f719
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-01 13:08:11 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -48,6 +48,40 @@
         </div>
     </div>
 </div>
+<div class="col-6 col-lg-3 col-md-6">
+    <div class="card" id="abrirModalTarjetas" style="cursor: pointer;">
+        <div class="card-body px-3 py-4-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="stats-icon blue">
+                        <i class="iconly-boldProfile"></i>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <h6 class="text-muted font-semibold">Followers</h6>
+                    <h6 class="font-extrabold mb-0">183.000</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-6 col-lg-3 col-md-6">
+    <div class="card" id="abrirModalTarjetas" style="cursor: pointer;">
+        <div class="card-body px-3 py-4-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="stats-icon blue">
+                        <i class="iconly-boldProfile"></i>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <h6 class="text-muted font-semibold">Followers</h6>
+                    <h6 class="font-extrabold mb-0">183.000</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
