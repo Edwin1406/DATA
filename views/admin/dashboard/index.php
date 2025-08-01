60f83b1151683836f53349856841a9886e00ed50
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-01 13:57:45 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -79,7 +79,7 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8">
-                                        <h6 class="text-muted font-semibold">Followers</h6>
+                                        <h6 class="text-muted font-semibold">CONSUMO GENERAL</h6>
                                         <h6 class="font-extrabold mb-0">183.000</h6>
                                     </div>
                                 </div>
