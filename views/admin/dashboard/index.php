7c837e58085a7e678cfac8daf40d42833ecb902b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-31 20:58:51 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -9,6 +9,31 @@
         <h3>ESTADISTICAS DEL PERFIL </h3>
     </div>
 
+    <div class="row">
+        <div class="col-12 col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Filtrar por Fecha</h4>
+                </div>
+                <div class="card-body">
+                    <form action="/admin/dashboard/filtrarPorFecha" method="POST">
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <label for="fechaInicio" class="form-label">Fecha Inicio</label>
+                                <input type="date" class="form-control" id="fechaInicio" name="fechaInicio" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="fechaFin" class="form-label">Fecha Fin</label>
+                                <input type="date" class="form-control" id="fechaFin" name="fechaFin" required>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Filtrar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="page-content">
         <section class="row">
             <div class="col-12 col-lg-9">
@@ -103,30 +128,7 @@
                 </div>
                 <!-- End of Profile Statistics -->
                 <!-- filtrador de fechas -->
-                <div class="row">
-                    <div class="col-12 col-lg-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Filtrar por Fecha</h4>
-                            </div>
-                            <div class="card-body">
-                                <form action="/admin/dashboard/filtrarPorFecha" method="POST">
-                                    <div class="row">
-                                        <div class="col-md-6 mb-3">
-                                            <label for="fechaInicio" class="form-label">Fecha Inicio</label>
-                                            <input type="date" class="form-control" id="fechaInicio" name="fechaInicio" required>
-                                        </div>
-                                        <div class="col-md-6 mb-3">
-                                            <label for="fechaFin" class="form-label">Fecha Fin</label>
-                                            <input type="date" class="form-control" id="fechaFin" name="fechaFin" required>
-                                        </div>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Filtrar</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
 
 
 
