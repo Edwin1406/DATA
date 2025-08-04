6d99c8c5ecbc16e01849378a2933abbe10e41099
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-03 22:31:17 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -16,10 +16,7 @@ class PruebasController
         echo "Controlador de Pruebas - Crear Prueba";
 
 
-        // Lógica para mostrar el formulario de creación de pruebas
-        $router->render('admin/pruebas/crearPrueba', [
-            'titulo' => 'Crear Prueba'
-        ]);
+     
     }
     
 
