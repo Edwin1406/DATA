0bd13c9dc3fa3cbc89fcb0e91367ae939103f198
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-05 13:51:01 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -236,7 +236,6 @@
             </div>
 
 
-            <!-- </div> -->
         </section>
     </div>
 
@@ -248,43 +247,46 @@
 
 <?php } ?>
 
+</div>
 
 
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="UTF-8">
-  <title>Gráfico Radial</title>
-  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <meta charset="UTF-8">
+    <title>Gráfico Radial</title>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </head>
+
 <body>
 
-  <div id="chart1"></div>
+    <div id="chart1"></div>
 
-  <script>
-    var options1 = {
-      chart: {
-        height: 280,
-        type: "radialBar",
-      },
-      series: [67, 84, 97, 61],
-      plotOptions: {
-        radialBar: {
-          dataLabels: {
-            total: {
-              show: true,
-              label: 'TOTAL'
-            }
-          }
-        }
-      },
-      labels: ['TEAM A', 'TEAM B', 'TEAM C', 'TEAM D']
-    };
+    <script>
+        var options1 = {
+            chart: {
+                height: 280,
+                type: "radialBar",
+            },
+            series: [67, 84, 97, 61],
+            plotOptions: {
+                radialBar: {
+                    dataLabels: {
+                        total: {
+                            show: true,
+                            label: 'TOTAL'
+                        }
+                    }
+                }
+            },
+            labels: ['TEAM A', 'TEAM B', 'TEAM C', 'TEAM D']
+        };
 
-    new ApexCharts(document.querySelector("#chart1"), options1).render();
-  </script>
+        new ApexCharts(document.querySelector("#chart1"), options1).render();
+    </script>
 
 </body>
+
 </html>
- 
