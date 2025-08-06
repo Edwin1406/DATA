a78d33e8278121e82e335282afb9a54eef09347e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-06 16:24:05 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -12,10 +12,23 @@ class PruebasController
 
     public static function crearPruebas(Router $router)
     {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+        }
+        $alertas = [];
 
+        // $control_guillotina = ControlGuillotina::all();
+
+        // NOMBRE DE LA PERSONA LOGEADA
+        $nombre = $_SESSION['nombre'];
+        $email = $_SESSION['email'];
         // Renderizar la vista de crear pruebas
         $router->render('admin/pruebas/crearPruebas', [
-            'titulo' => 'Crear Pruebas'
+            'titulo' => 'Crear Pruebas',
+            'alertas' => $alertas,
+            'nombre' => $nombre,
+            'email' => $email
         ]);
 
 
