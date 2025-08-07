e89208ee7731ee808574f8eb9dca9e7584efa762
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-07 15:12:11 -0500

    Update control_troquel.php

--- views/admin/control/control_troquel.php
+++ views/admin/control/control_troquel.php
@@ -103,8 +103,8 @@
                                         <label for="operador">Escoja el Operador</label>
                                         <div class="form-group">
                                             <select class="form-select" name="operador" id="operador">
-                                                <option value="Luis Govea">Luis Govea</option>
-                                                <option value="Guillermo Bonilla">Guillermo Bonilla</option>
+                                                <option value="Alvaro Pogo">Alvaro Pogo</option>
+                                                <option value="Evelin Oviedo">Evelin Oviedo</option>
                                             </select>
                                         </div>
                                     </div>
