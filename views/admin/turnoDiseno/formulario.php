b4c3abc709b9ebbb30feb74974c85f34397afea9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-12 12:43:53 -0500

    Update formulario.php

--- views/admin/turnoDiseno/formulario.php
+++ views/admin/turnoDiseno/formulario.php
@@ -2,7 +2,7 @@
 <div class="col-md-6 col-12">
     <div class="form-group">
         <label for="detalle">Detalle</label>
-        <textarea id="detalle" class="form-control form-control-lg" 
+        <textarea id="detalle" class="form-control form-control-sm" 
                   placeholder="Escribe el detalle aquí..." name="detalle" rows="4"><?php 
             echo isset($diseno) ? s($diseno->detalle) : ''; 
         ?></textarea>
