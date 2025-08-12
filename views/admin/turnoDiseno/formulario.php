2d0a8ea2206d72fca4e55e3e6d1b068778f6bf80
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-12 13:01:14 -0500

    Update formulario.php

--- views/admin/turnoDiseno/formulario.php
+++ views/admin/turnoDiseno/formulario.php
@@ -4,7 +4,7 @@
       <label for="detalle">Detalle</label>
       <textarea id="detalle" class="form-control form-control-sm"
         placeholder="Escribe el detalle aquí..." name="detalle" rows="4">
-        <?php echo isset($turno) ? s($turno->detalle) : '';?></textarea>
+        <?php echo isset($turno) ? s($turno->detalle) : ''; ?></textarea>
     </div>
   </div>
 
@@ -25,5 +25,27 @@
       <input type="text" id="observaciones" class="form-control"
         placeholder="Observaciones" name="observaciones"
         value="<?php echo isset($turno) ? s($turno->observaciones) : ''; ?>">
+    </div>
+  </div>
+  <!-- fecha de entrega -->
+  <div class="col-md-6 col-12">
+    <div class="form-group">
+      <label for="fecha_entrega">Fecha de Entrega</label>
+      <input type="text" id="fecha_entrega" class="form-control"
+        placeholder="Fecha de Entrega" name="fecha_entrega"
+        value="<?php echo isset($turno) ? s($turno->fecha_entrega) : ''; ?>">
+    </div>
+  </div>
+
+
+  <!-- estado -->
+  <div class="col-md-6 col-12">
+    <div class="form-group">
+      <label for="estado">Estado</label>
+      <select id="estado" class="form-control" name="estado">
+        <option value="PENDIENTE" <?php echo isset($turno) && $turno->estado === 'PENDIENTE' ? 'selected' : ''; ?>>Pendiente</option>
+        <option value="EN_PROCESO" <?php echo isset($turno) && $turno->estado === 'EN_PROCESO' ? 'selected' : ''; ?>>En Proceso</option>
+        <option value="FINALIZADO" <?php echo isset($turno) && $turno->estado === 'FINALIZADO' ? 'selected' : ''; ?>>Finalizado</option>
+      </select>
     </div>
   </div>
