0439723f6e5764ca2f8161f384fcaa777ab32574
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-12 15:57:16 -0500

    Update formulario.php

--- views/admin/turnoDiseno/formulario.php
+++ views/admin/turnoDiseno/formulario.php
@@ -33,26 +33,27 @@
   <!-- fecha de entrega -->
 
 
-         <?php if ($email !== 'ventas@megaecuador.com') { ?>
-  <div class="col-md-6 col-12">
-    <div class="form-group">
-      <label for="fecha_entrega">Fecha de Entrega</label>
-      <input type="date" id="fecha_entrega" class="form-control"
-        placeholder="Fecha de Entrega" name="fecha_entrega"
-        value="<?php echo isset($turno) ? s($turno->fecha_entrega) : ''; ?>">
+  <?php if ($email !== 'ventas@megaecuador.com') { ?>
+    <div class="col-md-6 col-12">
+      <div class="form-group">
+        <label for="fecha_entrega">Fecha y Hora de Entrega</label>
+        <input type="datetime-local" id="fecha_entrega" class="form-control"
+          placeholder="Fecha y Hora de Entrega" name="fecha_entrega"
+          value="<?php echo isset($turno) ? date('Y-m-d\TH:i', strtotime($turno->fecha_entrega)) : ''; ?>">
+      </div>
     </div>
-  </div>
 
-  <!-- estado -->
-  <div class="col-md-6 col-12">
-    <div class="form-group">
-      <label for="estado">Estado</label>
-      <select id="estado" class="form-control" name="estado">
-        <option value="" disabled <?php echo !isset($turno) ? 'selected' : ''; ?>>Seleccione un estado</option>
-        <option value="PENDIENTE" <?php echo isset($turno) && $turno->estado === 'PENDIENTE' ? 'selected' : ''; ?>>Pendiente</option>
-        <option value="EN_PROCESO" <?php echo isset($turno) && $turno->estado === 'EN_PROCESO' ? 'selected' : ''; ?>>En Proceso</option>
-        <option value="ENTREGADO" <?php echo isset($turno) && $turno->estado === 'ENTREGADO' ? 'selected' : ''; ?>>Entregado</option>
-      </select>
+
+    <!-- estado -->
+    <div class="col-md-6 col-12">
+      <div class="form-group">
+        <label for="estado">Estado</label>
+        <select id="estado" class="form-control" name="estado">
+          <option value="" disabled <?php echo !isset($turno) ? 'selected' : ''; ?>>Seleccione un estado</option>
+          <option value="PENDIENTE" <?php echo isset($turno) && $turno->estado === 'PENDIENTE' ? 'selected' : ''; ?>>Pendiente</option>
+          <option value="EN_PROCESO" <?php echo isset($turno) && $turno->estado === 'EN_PROCESO' ? 'selected' : ''; ?>>En Proceso</option>
+          <option value="ENTREGADO" <?php echo isset($turno) && $turno->estado === 'ENTREGADO' ? 'selected' : ''; ?>>Entregado</option>
+        </select>
+      </div>
     </div>
-  </div>
   <?php } ?>
