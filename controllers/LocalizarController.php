3883714dc94a910e92c1fce1f4b35b50b05aac6c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-14 15:56:05 -0500

    Update LocalizarController.php

--- controllers/LocalizarController.php
+++ controllers/LocalizarController.php
@@ -2,9 +2,7 @@
 
 namespace Controllers;
 
-use Model\Carrito;
-use Model\DetalleVenta;
-use Model\Ventas;
+
 use MVC\Router;
 
 
@@ -14,18 +12,22 @@ class LocalizarController
 
     public static function registroVehiculos(Router $router)
     {
+        session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+            exit;
+        }
+
+        // NOMBRE DE LA PERSONA LOGEADA
+        $nombre = $_SESSION['nombre'];
+        $email = $_SESSION['email'];
+
 
         $router->render('admin/control/vehiculos/registroVehiculos', [
             'titulo' => 'Registro de Vehículos',
+            'nombre' => $nombre,
+            'email' => $email
+
         ]);
-
-
     }
-
 }
-
-
-
-
-
-
