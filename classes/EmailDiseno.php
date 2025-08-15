405e0dc871d59c07688d58f9975a56c926a049de
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-15 10:22:32 -0500

    d

--- classes/EmailDiseno.php
+++ classes/EmailDiseno.php
@@ -8,13 +8,13 @@ class EmailDiseno {
 
     public $email;
     public $nombre;
-    public $turno->id;
-    
-    public function __construct($email, $nombre, $token)
+    public $turno_id;
+
+    public function __construct($email, $nombre, $turno_id)
     {
         $this->email = $email;
         $this->nombre = $nombre;
-        $this->token = $token;
+        $this->turno_id = $turno_id;
     }
 
     public function enviarConfirmacion() {
@@ -39,8 +39,8 @@ class EmailDiseno {
          $mail->CharSet = 'UTF-8';
 
          $contenido = '<html>';
-         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has Registrado Correctamente tu cuenta en Sitio Web; pero es necesario confirmarla</p>";
-         $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['HOST'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";       
+         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong>Se realizó un nuevo registro en el sistema.</p>";
+         $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['HOST'] . "/confirmar-cuenta?turno_id=" . $this->turno_id . "'>Confirmar Cuenta</a>";
          $contenido .= "<p>Si tu no creaste esta cuenta; puedes ignorar el mensaje</p>";
          $contenido .= '</html>';
          $mail->Body = $contenido;
@@ -50,34 +50,4 @@ class EmailDiseno {
 
     }
 
-    public function enviarInstrucciones() {
-
-        // create a new object
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = $_ENV['EMAIL_HOST'];
-        $mail->SMTPAuth = true;
-        $mail->Port = $_ENV['EMAIL_PORT'];
-        $mail->Username = $_ENV['EMAIL_USER'];
-        $mail->Password = $_ENV['EMAIL_PASS'];
-        $mail->SMTPSecure = 'ssl';
-    
-        $mail->setFrom('agrolecc@gmail.com');
-        $mail->addAddress($this->email, $this->nombre);
-        $mail->Subject = 'Reestablece tu password';
-
-        // Set HTML
-        $mail->isHTML(TRUE);
-        $mail->CharSet = 'UTF-8';
-
-        $contenido = '<html>';
-        $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['HOST'] . "/reestablecer?token=" . $this->token . "'>Reestablecer Password</a>";        
-        $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
-        $contenido .= '</html>';
-        $mail->Body = $contenido;
-
-        //Enviar el mail
-        $mail->send();
-    }
 }
