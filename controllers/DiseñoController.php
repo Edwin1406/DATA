c34419b0d5b09eae4f99e79df32c97c5c0d2974b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-15 12:56:14 -0500

    Update DiseñoController.php

--- controllers/DiseñoController.php
+++ controllers/DiseñoController.php
@@ -249,7 +249,7 @@ class DiseñoController
             $turno->codigo = substr(md5(uniqid(rand(), true)), 0, 6);
             // debuguear($turno);
                 // email por defecto
-            $emaildefault = 'desarrollodeproductosms@gmail.com';
+            $emaildefault = 'desarrollodeproductoms@gmail.com';
 
             // Enviar correo de confirmación
             $email = new EmailRegistroDiseno($emaildefault, $turno->vendedor, $turno->codigo, $turno->estado);
