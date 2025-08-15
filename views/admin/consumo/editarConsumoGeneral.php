d970e7ce270f619b08545dc09c95b5c32413e7e0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-15 11:42:04 -0500

    Update editarConsumoGeneral.php

--- views/admin/consumo/editarConsumoGeneral.php
+++ views/admin/consumo/editarConsumoGeneral.php
@@ -95,9 +95,6 @@
                                         </div>
                                     </div>
 
-                                 
-
-
                                     <div class="col-12 d-flex justify-content-end">
                                         <button type="submit" class="btn btn-primary me-1 mb-1">Atualizar</button>
                                         <button type="reset" class="btn btn-light-secondary me-1 mb-1">Limpiar</button>
