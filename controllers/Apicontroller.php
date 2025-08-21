54bb6961454e5d17321f773a3dfa8d940e015155
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-21 13:28:46 -0500

    Update Apicontroller.php

--- controllers/Apicontroller.php
+++ controllers/Apicontroller.php
@@ -8,6 +8,9 @@ use Model\Mantenimiento;
 class Apicontroller {
    
     public static function apiConsumoGeneral():void {
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="consumo_general.xlsx"');
+header('Cache-Control: max-age=0');
         $consmogeneral = Consumo_general::all('ASC');
         // Devolver los datos en formato JSON
         header('Content-Type: application/json');
