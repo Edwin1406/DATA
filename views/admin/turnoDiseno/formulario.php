e656f97e256f2b560ed89dd285a2ddcba7d12552
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-22 16:21:40 -0500

    Update formulario.php

--- views/admin/turnoDiseno/formulario.php
+++ views/admin/turnoDiseno/formulario.php
@@ -249,8 +249,8 @@
 <div class="col-md-6 col-12">
 
   <div class="form-group">
+    <label for="colores">Colores</label>
     <select class="choices form-select select-light-danger" multiple="multiple" name="colores[]">
-      <label for="colores">Colores</label>
       <option value="" disabled>Seleccione los colores</option>
       <option value="ROJO">ROJO</option>
       <option value="AZUL">AZUL</option>
