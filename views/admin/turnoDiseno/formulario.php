d9b4deb8217671c013ea360e2e2062f45033a45b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-08-25 15:09:00 -0500

    Update formulario.php

--- views/admin/turnoDiseno/formulario.php
+++ views/admin/turnoDiseno/formulario.php
@@ -247,7 +247,7 @@
 
 <!-- MAXIMO 4 COLORES ME DEJE ESOCGER  -->
 
-<div class="col-md-6 col-12">
+<!-- <div class="col-md-6 col-12">
   <div class="form-group">
     <label for="colores">Colores</label>
     <select class="choices form-select select-light-danger" multiple="multiple" name="colores[]">
@@ -260,7 +260,28 @@
       <option value="BLANCO">BLANCO</option>
     </select>
   </div>
+</div> -->
+
+
+
+<div class="col-md-6 col-12">
+  <div class="form-group">
+    <label for="colores">Colores</label>
+    <select class="choices form-select select-light-danger" multiple="multiple" name="colores[]">
+      <option value="" disabled>Seleccione los colores</option>
+      <option value="ROJO"     <?= in_array("ROJO", $coloresSeleccionados) ? "selected" : "" ?>>ROJO</option>
+      <option value="AZUL"     <?= in_array("AZUL", $coloresSeleccionados) ? "selected" : "" ?>>AZUL</option>
+      <option value="VERDE"    <?= in_array("VERDE", $coloresSeleccionados) ? "selected" : "" ?>>VERDE</option>
+      <option value="AMARILLO" <?= in_array("AMARILLO", $coloresSeleccionados) ? "selected" : "" ?>>AMARILLO</option>
+      <option value="NEGRO"    <?= in_array("NEGRO", $coloresSeleccionados) ? "selected" : "" ?>>NEGRO</option>
+      <option value="BLANCO"   <?= in_array("BLANCO", $coloresSeleccionados) ? "selected" : "" ?>>BLANCO</option>
+    </select>
+  </div>
 </div>
+
+
+
+
 
 <!-- fecha de entrega -->
 <?php if ($email !== 'ventas@megaecuador.com') { ?>
