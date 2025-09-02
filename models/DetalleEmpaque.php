2d073a63e61303483bb8cd798b5b873af6ea562f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-02 09:51:08 -0500

    Update DetalleEmpaque.php

--- models/DetalleEmpaque.php
+++ models/DetalleEmpaque.php
@@ -6,23 +6,23 @@ use DateTime;
 
 class DetalleEmpaque extends ActiveRecord {    
     protected static $tabla = 'tabla_resumen';
-    protected static $columnasDB = ['id','persona','horas_efectivas','registros','tiempos_muertos'];
+    protected static $columnasDB = ['id','persona','horas_efectivas','registros','tiempo_muerto'];
 
     public $id;
     public $persona;
     public $horas_efectivas;
     public $registros;
-    public $tiempos_muertos;
+    public $tiempo_muerto;
 
     public function __construct($data = []) {
         $this->id = $data['id'] ?? null;
         $this->persona = $data['persona'] ?? null;
         $this->horas_efectivas = $data['horas_efectivas'] ?? null;
         $this->registros = $data['registros'] ?? null;
-        $this->tiempos_muertos = $data['tiempos_muertos'] ?? null;
+        $this->tiempo_muerto = $data['tiempo_muerto'] ?? null;
     }
 
-    
+
 
 
 
