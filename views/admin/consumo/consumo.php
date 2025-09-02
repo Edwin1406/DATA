30243e778b123f24bb1dfbf36d3d31f6c9e890c8
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-02 09:13:21 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -80,7 +80,7 @@
                             <!-- agregar horas de trabajo con post porqe se guarda en otro formulario  -->
 
                             <!-- validar que sea el usario admin para poder agregar horas -->
-                            <?php if ($email == 'pruebas@megaecuador.com') { ?>
+                            <!-- <?php if ($email == 'pruebas@megaecuador.com') { ?>
                                 <form action="/admin/horas_trabajo" method="POST">
                                     <div class="row">
                                         <div class="col-md-2 col-12">
@@ -93,7 +93,7 @@
                                     </div>
                                     <button type="submit" class="btn btn-primary">Agregar Horas</button>
                                 </form>
-                            <?php } ?>
+                            <?php } ?> -->
 
 
 
