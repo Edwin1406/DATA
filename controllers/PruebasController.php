eb6f59cb0220b86648f65a876108906ed1fcdadc
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-03 15:39:47 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -34,7 +34,7 @@ class PruebasController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Procesar el formulario
             $carrito->id_usuario = $_SESSION['id'];
-            $carrito->tipo_maquina = $email;
+            $carrito->tipo_maquina = $nombre;
             $carrito->tipo_clasificacion = $_POST['tipo_clasificacion'];
             $carrito->cantidad = $_POST['cantidad'];
 
