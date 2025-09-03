1e51c13e089bfab8c86c6b0e96de248689636bfa
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-03 11:29:41 -0500

    Update crearPruebas.php

--- views/admin/pruebas/crearPruebas.php
+++ views/admin/pruebas/crearPruebas.php
@@ -78,7 +78,7 @@
                                 <div class="row">
 
 
-
+<!-- 
                                     <div class="col-md-6 col-12">
                                         <label for="id_producto">Escoja el Producto</label>
                                         <div class="form-group">
@@ -87,7 +87,7 @@
                                                 <option value="2">Producto 2</option>
                                             </select>
                                         </div>
-                                    </div>
+                                    </div> -->
 
 
                                     <div class="col-md-6 col-12">
