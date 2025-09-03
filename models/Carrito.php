539b18d347fb2ea0ba43a412464257466a29f45f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-03 15:16:18 -0500

    .

--- models/Carrito.php
+++ models/Carrito.php
@@ -7,11 +7,12 @@ namespace Model;
 class Carrito extends ActiveRecord {
 
     protected static $tabla = 'carrito';
-    protected static $columnasDB = ['id', 'id_usuario','tipo_maquina','cantidad','precio_unitario'];
+    protected static $columnasDB = ['id', 'id_usuario','tipo_maquina','tipo_clasificacion','cantidad','precio_unitario'];
 
     public $id;
     public $id_usuario;
     public $tipo_maquina;
+    public $tipo_clasificacion;
     public $cantidad;
     public $precio_unitario;
 
@@ -21,6 +22,7 @@ class Carrito extends ActiveRecord {
         $this->id = $args['id'] ?? null;
         $this->id_usuario = $args['id_usuario'] ?? null;
         $this->tipo_maquina = $args['tipo_maquina'] ?? null;
+        $this->tipo_clasificacion = $args['tipo_clasificacion'] ?? null;    
         $this->cantidad = $args['cantidad'] ?? 0;
         $this->precio_unitario = $args['precio_unitario'] ?? 0.0;
     }
