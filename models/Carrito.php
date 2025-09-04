8e8ee21759890e9b75173ca1d6a68eed6f55f8d1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-04 14:44:44 -0500

    Update Carrito.php

--- models/Carrito.php
+++ models/Carrito.php
@@ -7,7 +7,7 @@ namespace Model;
 class Carrito extends ActiveRecord {
 
     protected static $tabla = 'carrito';
-    protected static $columnasDB = ['id', 'id_usuario','tipo_maquina','tipo_clasificacion','casos','cantidad','metros_lineales','n_laminas','n_cambios','consumo_almidon','consumo_resina','consumo_recubrimiento'];
+    protected static $columnasDB = ['id', 'id_usuario','tipo_maquina','tipo_clasificacion','casos','cantidad'];
 
     public $id;
     public $id_usuario;
@@ -15,12 +15,7 @@ class Carrito extends ActiveRecord {
     public $tipo_clasificacion;
     public $casos;
     public $cantidad;
-    public $metros_lineales;
-    public $n_laminas;
-    public $n_cambios;
-    public $consumo_almidon;
-    public $consumo_resina;
-    public $consumo_recubrimiento;
+    
     
 
 
@@ -32,17 +27,10 @@ class Carrito extends ActiveRecord {
         $this->tipo_clasificacion = $args['tipo_clasificacion'] ?? null;
         $this->casos = $args['casos'] ?? null;
         $this->cantidad = $args['cantidad'] ?? 0;
-        $this->metros_lineales = $args['metros_lineales'] ?? 0;
-        $this->n_laminas = $args['n_laminas'] ?? 0;
-        $this->n_cambios = $args['n_cambios'] ?? 0;
-        $this->consumo_almidon = $args['consumo_almidon'] ?? 0;
-        $this->consumo_resina = $args['consumo_resina'] ?? 0;
-        $this->consumo_recubrimiento = $args['consumo_recubrimiento'] ?? 0;
-
     }
 
 
-    
+
 
 
 
