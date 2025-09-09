12525f32f154be558a1dad40c16496fd30ed4c55
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-08 19:14:45 -0500

    Update consumo_general.php

--- views/admin/consumo/consumo_general.php
+++ views/admin/consumo/consumo_general.php
@@ -91,7 +91,7 @@
                                                 <option value="DOBLADO">DOBLADO</option>
                                                 <option value="CORTE CEJA">CORTE CEJA</option>
                                                 <option value="CONVERTIDOR">CONVERTIDOR</option>
-                                                <option value="GUILLOTINA LAMINA">GUILLLOTINA LAMINA</option>
+                                                <option value="GUILLOTINA LAMINA">GUILLOTINA LAMINA</option>
                                                 <option value="GUILLOTINA PAPEL">GUILLOTINA PAPEL</option>
                                                 <option value="EMPAQUE">EMPAQUE</option>
                                                 <option value="BODEGA">BODEGA</option>
