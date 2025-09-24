b28801de9c4694953ed9a6ab4d57c0e4fb7d9d14
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-24 12:41:30 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -158,7 +158,7 @@
                                     <div class="col-md-6 col-12">
                                         <div class="form-group">
                                             <label for="n_orden">Número de Orden</label>
-                                            <input type="number" id="n_orden" class="form-control" placeholder="Número de Orden" name="n_orden">
+                                            <input type="text" id="n_orden" class="form-control" placeholder="Número de Orden" name="n_orden">
                                         </div>
                                     </div>
 
