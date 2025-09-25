d9547f281524af52a5ad6e29c752641f881c5464
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-25 11:14:32 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -205,7 +205,7 @@
 
 
                                     <!-- Select2 -->
-                                    <!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+                                    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
                                     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
                                     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
@@ -217,7 +217,7 @@
                                                 allowClear: true
                                             });
                                         });
-                                    </script> -->
+                                    </script>
 
 
 
