c2702f836eb3778919142974a17c43e2f2208468
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-28 22:59:57 -0500

    Update produccion_diaria.php

--- views/admin/diaria/produccion_diaria.php
+++ views/admin/diaria/produccion_diaria.php
@@ -69,7 +69,7 @@
                     </div>
                     <div class="card-content">
                         <div class="card-body">
-                            <form class="form" method="POST" action="/admin/consumo/produccion_diaria" enctype="multipart/form-data">
+                            <form class="form" method="POST" action="/admin/diaria/produccion_diaria" enctype="multipart/form-data">
                                 <div class="row">
 
                                       <?php include_once __DIR__.'/formulariodiaria.php'  ?>
