7251831a6d1118f698b39b592509d240cde528ac
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-30 17:05:56 -0500

    Update crearPruebas.php

--- views/admin/pruebas/crearPruebas.php
+++ views/admin/pruebas/crearPruebas.php
@@ -258,7 +258,7 @@
                             <div class="form-group">
                                 <label for="fecha">Fecha</label>
                                 <input type="date" id="fecha" class="form-control"
-                                    name="fecha" value="<?php echo date('Y-m-d'); ?>" required>
+                                    name="fecha" value="<?php echo date('Y-m-d'); ?>" readonly>
                             </div>
                         </div>
 
