b23281fd63999d48f574efded21e8860b0a65a5f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-30 14:37:55 -0500

    Update formulariodiaria.php

--- views/admin/diaria/formulariodiaria.php
+++ views/admin/diaria/formulariodiaria.php
@@ -145,6 +145,8 @@
 
 
  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
   <script>
 
 $(document).ready(function() {
