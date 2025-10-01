bde00faa0631650c4c5078cb9497bfebc33b00b6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-01 16:18:17 -0500

    Update editarPruebas.php

--- views/admin/pruebas/editarPruebas.php
+++ views/admin/pruebas/editarPruebas.php
@@ -95,8 +95,7 @@
                                 <label for="n_laminas">N° de Laminas</label>
                                 <input type="number" id="n_laminas" class="form-control"
                                     placeholder="N° de Laminas" name="n_laminas"
-                                    value="<?php echo $venta->n_laminas ?? ''; ?>"
-                                    >
+                                    value="<?php echo $venta->n_laminas ?? ''; ?>">
                             </div>
                         </div>
 
@@ -107,8 +106,7 @@
                                 <label for="metros_lineales">Metros Lineales</label>
                                 <input type="number" id="metros_lineales" class="form-control"
                                     placeholder="Metros Lineales" name="metros_lineales"
-                                    value="<?php echo $venta->metros_lineales ?? ''; ?>"
-                                    >
+                                    value="<?php echo $venta->metros_lineales ?? ''; ?>">
                             </div>
 
                         </div>
@@ -121,8 +119,7 @@
                                 <label for="turno">Turno</label>
                                 <input type="time" id="turno" class="form-control"
                                     placeholder="Turno" name="turno"
-                                    value="<?php echo $venta->turno ?? ''; ?>"
-                                    >
+                                    value="<?php echo $venta->turno ?? ''; ?>">
                             </div>
 
                         </div>
@@ -132,7 +129,7 @@
                             <div class="form-group">
                                 <label for="n_cambios">N° de Cambios</label>
                                 <input type="number" id="n_cambios" class="form-control"
-                                    placeholder="N° de Cambios" 
+                                    placeholder="N° de Cambios"
                                     value="<?php echo $venta->n_cambios ?? ''; ?>"
                                     name="n_cambios">
                             </div>
@@ -144,7 +141,7 @@
                             <div class="form-group">
                                 <label for="hora_inicio">Hora Inicio</label>
                                 <input type="time" id="hora_inicio" class="form-control"
-                                    name="hora_inicio" 
+                                    name="hora_inicio"
                                     value="<?php echo $venta->hora_inicio ?? ''; ?>"
                                     required>
 
@@ -156,7 +153,7 @@
                             <div class="form-group">
                                 <label for="hora_fin">Hora Fin</label>
                                 <input type="time" id="hora_fin" class="form-control"
-                                    name="hora_fin" 
+                                    name="hora_fin"
                                     value="<?php echo $venta->hora_fin ?? ''; ?>"
                                     required>
                             </div>
@@ -185,12 +182,17 @@
                         <div class="col-md-5 col-12">
                             <div class="form-group">
                                 <label for="motivo_inactividad">Motivo de la Inactividad</label>
-                                <textarea id="motivo_inactividad" class="form-control" name="motivo_inactividad" placeholder="Describe el motivo de la inactividad" rows="3" ><?php echo $venta->motivo_inactividad ?? ''; ?></textarea>
+                                <textarea id="motivo_inactividad" class="form-control" name="motivo_inactividad" placeholder="Describe el motivo de la inactividad" rows="3"><?php echo $venta->motivo_inactividad ?? ''; ?></textarea>
                             </div>
                         </div>
 
 
-
+                        <div class="form-group">
+                            <label for="unidades_pendientes">Unidades x procesar </label>
+                            <input type="number" id="unidades_pendientes" class="form-control"
+                                placeholder="Unidades x procesar" name="unidades_pendientes"
+                                value="<?php echo $venta->unidades_pendientes ?? ''; ?>">
+                        </div>
 
                     </div>
 
