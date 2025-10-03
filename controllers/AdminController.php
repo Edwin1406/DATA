fff8c52c2a8346bec9cc659fd2488b657535af5b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-03 08:56:58 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -616,7 +616,12 @@ class AdminController
 
             $produccion_diaria->fecha = $_POST['fecha'] ?? null;
 
-          $produccion_diaria->metros_lineales = $_POST['metros_lineales'] =  0 ?? null;
+            if ($produccion_diaria->linea === 'CORRUGADOR CAJAS' || $produccion_diaria->linea === 'CORRUGADOR PLANCHAS') {
+    $produccion_diaria->metros_lineales = $_POST['metros_lineales'] =  0 ?? null;
+}
+
+
+          
 
 
 
