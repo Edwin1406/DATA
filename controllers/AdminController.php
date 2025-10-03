4fc95422ca8d40cd9d78332253c107d239d4a0ea
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-03 09:57:16 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -651,7 +651,7 @@ class AdminController
             $alertas = $produccion_diaria->validar();
             if (empty($alertas)) {
                 $produccion_diaria->guardar();
-                header('Location: /admin/diaria/produccion_diaria?exito=1');
+                header('Location: /admin/diaria/produccion_diaria?id=' . $produccion_diaria->id_corrugador . '&exito=1');
             }
         } else {
             $alertas = [];
