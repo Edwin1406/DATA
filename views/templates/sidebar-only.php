35d36142fb6df5e8050f644bca76c808e9be4613
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-03 13:24:58 -0500

    s

--- views/templates/sidebar-only.php
+++ views/templates/sidebar-only.php
@@ -79,7 +79,7 @@
                              <?php }  ?>
 
 
-
+                                
                              <?php if ($email !== 'planta@megaecuador.com') { ?>
                                  <li class="submenu-item ">
                                      <a href="/admin/diaria/produccion_diaria"><i class="bi bi-arrow-right"> </i>Producción Diaria</a>
@@ -203,13 +203,13 @@
                          </a>
                          <ul class="submenu ">
                              <?php if ($userEmail !== 'corrugador@megaecuador.com') { ?>
-                                 <li class="submenu-item ">
-                                     <a href="/admin/pruebas/tablaPruebas"><i class="bi bi-arrow-right"> </i>Tabla Pruebas</a>
-                                 </li>
-                                 <li class="submenu-item ">
-                                     <a href="/admin/pruebas/crearPruebas"><i class="bi bi-arrow-right"> </i>Crear Corrgador</a>
-                                 </li>
-                             <?php } ?>
+                             <li class="submenu-item ">
+                                 <a href="/admin/pruebas/tablaPruebas"><i class="bi bi-arrow-right"> </i>Tabla Pruebas</a>
+                             </li>
+                             <li class="submenu-item ">
+                                 <a href="/admin/pruebas/crearPruebas"><i class="bi bi-arrow-right"> </i>Crear Corrgador</a>
+                             </li>
+                                <?php } ?>
                              <li class="submenu-item ">
                                  <a href="/admin/pruebas/crearFlexo"><i class="bi bi-arrow-right"> </i>Crear Flexo</a>
                              </li>
@@ -241,76 +241,3 @@
      </div>
  </div>
 
-
-
- <div class="form-group">
-
-
-     <!-- Button trigger for login form modal -->
-     <button type="button" class="btn btn-outline-success" data-bs-toggle="modal"
-         data-bs-target="#inlineForm">
-         Agregar desperdicio corrugador
-     </button>
-
-     <!--login form Modal -->
-     <div class="modal fade text-left" id="inlineForm" tabindex="-1"
-         role="dialog" aria-labelledby="myModalLabel33" aria-hidden="true">
-         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
-             role="document">
-             <div class="modal-content">
-                 <div class="modal-header">
-                     <h4 class="modal-title" id="myModalLabel33">Login Form </h4>
-                     <button type="button" class="close" data-bs-dismiss="modal"
-                         aria-label="Close">
-                         <i data-feather="x"></i>
-                     </button>
-                 </div>
-                 <form action="#">
-                     <div class="modal-body">
-                         <label>Email: </label>
-                         <div class="form-group">
-                             <input type="text" placeholder="Email Address"
-                                 class="form-control">
-                         </div>
-                         <label>Password: </label>
-                         <div class="form-group">
-                             <input type="password" placeholder="Password"
-                                 class="form-control">
-                         </div>
-                     </div>
-                     <div class="modal-footer">
-                         <button type="button" class="btn btn-light-secondary"
-                             data-bs-dismiss="modal">
-                             <i class="bx bx-x d-block d-sm-none"></i>
-                             <span class="d-none d-sm-block">Close</span>
-                         </button>
-                         <button type="button" class="btn btn-primary ml-1"
-                             data-bs-dismiss="modal">
-                             <i class="bx bx-check d-block d-sm-none"></i>
-                             <span class="d-none d-sm-block">login</span>
-                         </button>
-                     </div>
-                 </form>
-             </div>
-         </div>
-     </div>
-
-
-
-
- <div class="card">
-     <div class="card-content">
-         <div class="card-body">
-
-         </div>
-     </div>
-
-
-
-     <section id="form-and-scrolling-components">
-         <div class="row">
-             <div class="col-md-3 col-12">
-
-             </div>
-         </div>
-     </section>
