40e3b722765ed7c6cc24883a8d21f1aa3f5f7048
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-06 12:17:32 -0500

    Update tablaPruebas.php

--- views/admin/pruebas/tablaPruebas.php
+++ views/admin/pruebas/tablaPruebas.php
@@ -113,7 +113,7 @@
                              <th class="fs-6" style="min-width: 80px;">Consumo Papel</th>
                              <th class="fs-6" style="min-width: 80px;">N° Láminas</th>
                              <th class="fs-6" style="min-width: 80px;">Turno</th>
-                             <th class="fs-6" style="min-width: 100px;">Motivo de Inactividad</th>
+                             <th class="fs-6" style="min-width: 60px;">Motivo de Inactividad</th>
 
                              <th class="fs-6" style="min-width: 100px;">Acciones</th>
                          </tr>
