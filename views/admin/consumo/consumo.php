4990a11b7e1f8cd3b174ba5c9b394fcd3ee3ccd1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-08 09:03:33 -0500

    Update consumo.php

--- views/admin/consumo/consumo.php
+++ views/admin/consumo/consumo.php
@@ -148,6 +148,7 @@
                                                 <option value="MARTIN CABEZAS">MARTIN CABEZAS</option>
                                                 <option value="VELENTIN PÉREZ">VELENTIN PÉREZ</option>
                                                 <option value="ADRIAN PILLAJO">ADRIAN PILLAJO</option>
+                                                <option value="SEGUNDO MENDOZA">SEGUNDO MENDOZA</option>
 
                                             </select>
                                         </div>
