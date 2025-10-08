272e2e6049a30cae8405f0b43a5fb7024048472c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-08 10:54:40 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -623,7 +623,15 @@ class AdminController
             $produccion_diaria->hora_inicio = $_POST['hora_inicio'] ?? null;
             $produccion_diaria->hora_fin = $_POST['hora_fin'] ?? null;
             $produccion_diaria->desperdicio_lamina = $_POST['desperdicio_lamina'] ?? 0;
+            $produccion_diaria->kilos_x_dia = $_POST['kilos_x_dia'] ?? 0;
+            $produccion_diaria->turno = $_POST['turno'] ?? null;
+            $produccion_diaria->horas_maquina = $_POST['horas_maquina'] ?? 0;
+            $produccion_diaria->cambios = $_POST['cambios'] ?? 0;
+            $produccion_diaria->tiempo_x_cambio = $_POST['tiempo_x_cambio'] ?? 0;
+            $produccion_diaria->unidades_x_procesar = $_POST['unidades_x_procesar'] ?? 0;
+            $produccion_diaria->kilos_x_procesar = $_POST['kilos_x_procesar'] ?? 0;
             
+
 
 
 
