0e7cf07bac2c7a9c95326e1df52523538482b4a3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-08 13:06:16 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -655,7 +655,7 @@ class AdminController
             
             $produccion_diaria->sincronizar($_POST);
             
-            debuguear($produccion_diaria);
+            // debuguear($produccion_diaria);
 
             // Verifica si la unidad por día no es cero
             if ($produccion_diaria->unidad_x_dia != 0) {
