5f6dfd4b8c02c1b9f104c2e02c51fb0c6aaa9616
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-08 11:06:19 -0500

    Update ProduccionDiaria.php

--- models/ProduccionDiaria.php
+++ models/ProduccionDiaria.php
@@ -16,6 +16,7 @@ class ProduccionDiaria extends ActiveRecord {
         'kilos_x_dia',
         'hora_inicio',
         'hora_fin',
+        'desperdicio_lamina',
         'turno',
         'horas_maquina',
         'cambios',
