74527a813867e45fa0009e2e005bf763f29057ae
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-08 11:18:36 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -600,6 +600,7 @@ class AdminController
         $email = $_SESSION['email'];
 
         $id = $_GET['id'] ?? null;
+        debuguear($id);
 
 
         $corrugador = Ventas::where('id', $id);
@@ -675,8 +676,6 @@ class AdminController
         //     $alertas = [];
         // }
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        unset($_POST['id']); // <- clave
-
     // 1) Sincroniza primero lo que venga del form
     $produccion_diaria->sincronizar($_POST);
 
