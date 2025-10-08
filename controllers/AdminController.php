fc91d79e0162112686c221ba6639eb074ab92f85
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-08 11:11:53 -0500

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -675,6 +675,8 @@ class AdminController
         //     $alertas = [];
         // }
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        unset($_POST['id']); // <- clave
+
     // 1) Sincroniza primero lo que venga del form
     $produccion_diaria->sincronizar($_POST);
 
