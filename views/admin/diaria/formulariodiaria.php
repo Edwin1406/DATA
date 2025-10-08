318851d7f3e769f2231fcbef004a72578f0493c5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-08 17:58:54 -0500

    Update formulariodiaria.php

--- views/admin/diaria/formulariodiaria.php
+++ views/admin/diaria/formulariodiaria.php
@@ -106,7 +106,7 @@
       <label for="horas_maquina">Horas de Maquina</label>
       <input type="time" id="horas_maquina" class="form-control"
         placeholder="Horas de Maquina" name="horas_maquina"
-        value="<?php echo isset($produccion_diaria) ? s($produccion_diaria->turno - 1) : ''; ?>">
+        value="<?php echo isset($turnoCorrugador1) ? s($turnoCorrugador1) : ''; ?>">
     </div>
   </div>
 
