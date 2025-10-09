304cc010bc91f5a6813a3aca6a1dcefff7b831e0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-09 15:42:45 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -572,7 +572,7 @@ class PruebasController
 
         // Obtener los diseños de la base de datos
         // $corrugador = DetalleVenta::wherenuevo('tipo_maquina', 'CORRUGADOR');
-        $corrugador = Ventas::all();
+        $corrugador = Ventas::whereArray('tipo_maquina', 'CORRUGADOR');
 
 
         // debuguear($corrgador);
