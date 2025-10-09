7f283ad8a4e1f269e8f07e43481958b94145a67e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-09 16:04:51 -0500

    Update tablaMicro.php

--- views/admin/pruebas/micro/tablaMicro.php
+++ views/admin/pruebas/micro/tablaMicro.php
@@ -89,7 +89,7 @@
 
                  <?php if ($email !== 'ventas@megaecuador.com') { ?>
                      <li class="nav-item">
-                         <a class="nav-link active" href="/admin/pruebas/crearPruebas">Registro Pruebas</a>
+                         <a class="nav-link active" href="/admin/pruebas/crearMicro">Registro Micro</a>
                      </li>
                  <?php } ?>
              </ul>
