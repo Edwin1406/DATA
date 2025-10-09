f2f2dbc9ee1030419d121fb8a71ea1cc01dd3136
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-09 17:20:04 -0500

    Update formulariodiaria.php

--- views/admin/diaria/formulariodiaria.php
+++ views/admin/diaria/formulariodiaria.php
@@ -20,13 +20,13 @@
     <div class="form-group">
       <label for="linea">Linea de Producción</label>
       <select class="form-select" name="linea" id="linea">
-        <!-- <option value="" disabled selected>-- Seleccione --</option>
-        <option value="MICRO" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'MICRO' ? 'selected' : ''; ?>>Micro</option>
-        <option value="SEPARADORES" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'SEPARADORES' ? 'selected' : ''; ?>>Separadores</option>
-        <option value="PERIODICO" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'PERIODICO' ? 'selected' : ''; ?>>Periódico</option> -->
+        <option value="" disabled selected>-- Seleccione --</option>
         <option value="CORRUGADOR PLANCHAS/CAJAS" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'CORRUGADOR PLANCHAS/CAJAS' ? 'selected' : ''; ?>>Corrugado Planchas/Cajas</option>
         <option value="CORRUGADOR PLANCHAS" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'CORRUGADOR PLANCHAS' ? 'selected' : ''; ?>>Corrugado de Planchas</option>
         <option value="CORRUGADOR CAJAS" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'CORRUGADOR CAJAS' ? 'selected' : ''; ?>>Corrugado de Cajas</option>
+        <option value="MICRO" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'MICRO' ? 'selected' : ''; ?>>Micro</option>
+        <option value="SEPARADORES" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'SEPARADORES' ? 'selected' : ''; ?>>Separadores</option>
+        <option value="PERIODICO" <?php echo isset($produccion_diaria) && s($produccion_diaria->linea) === 'PERIODICO' ? 'selected' : ''; ?>>Periódico</option>
       </select>
     </div>
   </div>
