c20ebd8a544d9dd9f82740f7261f70023c98755f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-21 13:57:18 -0500

    Update crearDesperdicios.php

--- views/admin/pruebas/desperdicios/crearDesperdicios.php
+++ views/admin/pruebas/desperdicios/crearDesperdicios.php
@@ -93,9 +93,6 @@
                                 </tr>
                             </thead>
 
-                         
-
-
                             <tbody>
                                 <?php foreach ($desperdicios as $desperdicio): ?>
                                     <tr>
@@ -114,36 +111,6 @@
                                 <?php endforeach; ?>
                             </tbody>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-                            <tfoot>
-                                <tr>
-                                    <th colspan="4" class="text-end">Total:</th>
-                                    <th>
-                                        <?php
-                                        $totalCantidad = 0;
-                                        foreach ($carritoTemporal as $contro) {
-                                            if ($tipo_maqina === $contro->tipo_maquina) {
-                                                $totalCantidad += $contro->cantidad;
-                                            }
-                                        }
-                                        echo number_format($totalCantidad, 2);
-                                        ?>
-                                    </th>
-                                    <th colspan="2"></th>
-                                </tr>
-                            </tfoot>
                         </table>
                     </div>
 
