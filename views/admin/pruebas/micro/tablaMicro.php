119a8f6cfe2d07fe78df051526af754895e0f665
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-22 16:11:24 -0500

    Update tablaMicro.php

--- views/admin/pruebas/micro/tablaMicro.php
+++ views/admin/pruebas/micro/tablaMicro.php
@@ -138,27 +138,12 @@
                                          <!-- desabilitar el boton si ya existe en prodccion diaria ese mismo id de registro  -->
                                          <?php if (isset($produccioduccionMicro) && in_array($microS->id, array_column($produccioduccionMicro, 'id_corrugador'))) : ?>
 
-                                             <button class="btn btn-secondary btn-sm" disabled>Diaria</button>
-                                         <?php else : ?>
-                                             <a href="/admin/diaria/produccion_diariaMicro?id_micro=<?= $produccioduccionMicro->id ?>" class="btn btn-primary btn-sm">Diaria</a>
+                                            <a href="/admin/diaria/produccion_diariaMicro?id_micro=<?= $produccioduccionMicro->id ?>" class="btn btn-primary btn-sm">Diaria</a>
+                                            <?php else : ?>
+                                                <button class="btn btn-secondary btn-sm" d >Diaria</button>
                                          <?php endif; ?>
 
-                                                <p>
-    <?php
-    if ($index !== false) {
-        echo $produccioduccionMicro[$index]['id'];
-    } else {
-        echo "—";
-    }
-    ?>
-</p>
-
-
-
-
-
-
-
+                                                
 
                                          <form action="/admin/eliminarMicro" method="POST">
                                              <input type="hidden" name="id" value="<?= $microS->id ?>">
