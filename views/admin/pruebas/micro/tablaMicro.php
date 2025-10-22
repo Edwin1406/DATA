86a607cb1c9fc802404ecf56ca6e7633d5386480
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-22 15:57:13 -0500

    Update tablaMicro.php

--- views/admin/pruebas/micro/tablaMicro.php
+++ views/admin/pruebas/micro/tablaMicro.php
@@ -140,7 +140,7 @@
 
                                             <button class="btn btn-secondary btn-sm" disabled>Diaria</button>
                                             <?php else : ?>
-                                                <a href="/admin/diaria/produccion_diariaMicro?id_micro=<?= $microS->id ?>" class="btn btn-primary btn-sm">Diaria</a>
+                                                <a href="/admin/diaria/produccion_diariaMicro?id_micro=<?= $produccioduccionMicro->id ?>" class="btn btn-primary btn-sm">Diaria</a>
                                             <?php endif; ?>
 
                                          <form action="/admin/eliminarMicro" method="POST">
