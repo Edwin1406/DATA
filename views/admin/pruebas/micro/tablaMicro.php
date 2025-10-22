68882e3388adf614f30b563bcd8996b51e6cdf08
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-22 16:37:55 -0500

    Update tablaMicro.php

--- views/admin/pruebas/micro/tablaMicro.php
+++ views/admin/pruebas/micro/tablaMicro.php
@@ -163,7 +163,7 @@
                                          <?php else: ?>
                                              <a href="/admin/diaria/editarproduccion_diariaMicro?id=<?= htmlspecialchars($idRegistro) ?>"
                                                  class="btn btn-secondary btn-sm">
-                                                 +
+                                                  +
                                              </a>
                                          <?php endif; ?>
 
