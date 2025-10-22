d79f8ce5aa94a09e2787e05eed1b2235ff87454c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-22 08:49:24 -0500

    Update crearDoblado.php

--- views/admin/pruebas/doblado/crearDoblado.php
+++ views/admin/pruebas/doblado/crearDoblado.php
@@ -324,7 +324,7 @@
                         </div>
 
                         <!-- fecha fin -->
-                        <div class="col-md-1 col-12">
+                        <div class="col-md-1.5 col-12">
                             <div class="form-group">
                                 <label for="hora_fin">Hora Fin</label>
                                 <input type="time" id="hora_fin" class="form-control"
@@ -332,7 +332,7 @@
                             </div>
                         </div>
 
-                        <div class="col-md-2.5 col-12">
+                        <div class="col-md-2 col-12">
                             <div class="form-group">
                                 <label for="operador">OPERADOR</label>
                                 <select id="operador" class="choices form-control" name="operador">
