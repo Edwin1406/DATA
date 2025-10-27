27c29323f600a7e6f41b87d021b657a4e63d61cb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-27 12:39:31 -0500

    Update tablaPrePrinter.php

--- views/admin/pruebas/preprinter/tablaPrePrinter.php
+++ views/admin/pruebas/preprinter/tablaPrePrinter.php
@@ -135,7 +135,7 @@
 
                                      <div class="d-flex gap-1">
                                          <a href="/admin/pruebas/editarPrePrinter?id=<?= $preprint->id ?>" class="btn btn-primary btn-sm">Editar</a>
-                                         <a href="/admin/pruebas/crearDesperdicios?id_venta=<?= $preprint->id ?>" class="btn btn-primary btn-sm">Desperdicios</a>
+                                         <!-- <a href="/admin/pruebas/crearDesperdicios?id_venta=<?= $preprint->id ?>" class="btn btn-primary btn-sm">Desperdicios</a> -->
                                          <button class="btn btn-danger btn-sm eliminar-btn"
                                              data-id="<?= $preprint->id ?>">Eliminar</button>
                                      </div>
